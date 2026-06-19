22d76089badeeb0c9c23a35708f89cf7e9978287
Mohamed Ali El Mellah <mellahmohamedali@gmail.com>
2026-06-19 04:59:31 +0100

    Deploy jumphost infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,81 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc-name" {
+  description = "VPC Name for our Jumphost server"
+  type        = string
+  default     = "Jumphost-vpc"
+}
+
+variable "igw-name" {
+  description = "Internet Gate Way Name for our Jumphost server"
+  type        = string
+  default     = "Jumphost-igw"
+}
+
+variable "subnet-name" {
+  description = "Subnet Name for our Jumphost server"
+  type        = string
+  default     = "Jumphost-subnet"
+}
+
+variable "rt-name" {
+  description = "Route Table Name for our Jumphost server"
+  type        = string
+  default     = "Jumphost-rt"
+}
+
+variable "sg-name" {
+  description = "Security Group for our Jumphost server"
+  type        = string
+  default     = "Jumphost-sg"
+}
+
+
+variable "iam-role" {
+  description = "IAM Role for the Jumphost Server"
+  type        = string
+  default     = "Jumphost-iam-role"
+}
+
+/*
+  How to get the latest Amazon Linux 2023 AMI:
+
+  aws ec2 describe-images \
+    --owners amazon \
+    --region us-east-1 \
+    --filters "Name=name,Values=al2023-ami-*-x86_64" "Name=state,Values=available" \
+    --query "Images | sort_by(@, &CreationDate)[-1].ImageId" \
+    --output text 
+
+  Copy the returned AMI ID and update variables.tf.
+*/
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-03d84abcde942cf8c" // Replace with the latest AMI ID for your region
+
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.large"
+}
+
+variable "key_name" {
+  description = "EC2 keypair"
+  type        = string
+  default     = "mykey-pair"
+}
+
+variable "instance_name" {
+  description = "EC2 Instance name for the jumphost server"
+  type        = string
+  default     = "Jumphost-server"
+}
+#
