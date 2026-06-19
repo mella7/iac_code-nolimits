a546621a01e35e516633b79c1d8c50a226e9dbc0
Mohamed Ali El Mellah <mellahmohamedali@gmail.com>
2026-06-19 05:27:48 +0100

    fix region to eu-north-1

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "nolimits-bucket-01 "
     key    = "terraform/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-north-1"
   }
 
   required_version = ">= 1.6.3"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-north-1"
 }
 
 variable "vpc-name" {
@@ -57,7 +57,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-03d84abcde942cf8c" // Replace with the latest AMI ID for your region
+  default     = "ami-088fd024fe7aa5070" // Replace with the latest AMI ID for your region
 
 }
 
